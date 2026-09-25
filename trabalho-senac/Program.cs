4a03f19ec4c6a19c7eb9cc21fbda71744bd8a225
bianca <biancaalison782@gmail.com>
2026-09-24 23:46:00 -0300

    Adiciona testes de Item Carrinho

--- trabalho-senac/Program.cs
+++ trabalho-senac/Program.cs
@@ -24,7 +24,38 @@ internal class Program
             20,
             200
             );
+    
+
+       //Testando ITEMCARRINHO BIANCA
+    
+        //testando items e vendo como funciona o subtotal pra nao perder logica
+        //por exemplo no carrinho vai ter o item teclado porem o cliente colocou
+        //que quer colocar 5 unidade do teclado , e entao o subtotal do item
+        // vai ser 5* 250 = 1250
+        ItemCarrinho item1 = new ItemCarrinho(1, "Teclado Gamer", 250.0, 5);
+        ItemCarrinho item2 = new ItemCarrinho(2, "Aspirador de pó", 500.0, 2);
+        ItemCarrinho item3 = new ItemCarrinho(3, "Placa de vídeo RTX 3050", 1000.0, 1);
+
+        Console.WriteLine(item1.CalcularSubtotal());
+
+        //testando atualizarQuantidade
+
+        ItemCarrinho item4 = new ItemCarrinho(4, "Processador intel i9", 1500.0, 2);
+        Console.WriteLine($"produto: {item4.NomeProduto}" );
+        Console.WriteLine($"Quantidade: {item4.Quantidade}" );
+        Console.WriteLine($"valor unitario: {item4.PrecoUnitario}");
+
+        item4.AtualizarQuantidade(4);
+
+        Console.WriteLine($"Nova quantidade: {item4.Quantidade}");
+        Console.WriteLine($"novo subtotal: {item4.CalcularSubtotal()}");
+
+        //se na nova quantidade for menor ou igual a 0 deve dar retorno ou seja muda nada
+        // fica no mesmo valor que estava antes
+
+        item1.AtualizarQuantidade(0);
+        Console.WriteLine($"Quantidade retornada: {item1.Quantidade}");
+
+
     }
 }
-
-    
